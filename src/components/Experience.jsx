204fc31f60ff9ee3a4a438e7ec2880f1cd402140
Manindar Kumar Singh <manindar2312@gmail.com>
2024-09-08 09:56:55 +0530

    Assignment by Lunacal

---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <>
+      <div className="bg-[#363C43] text-white mx-12 text-xl">
+        <h1 className="mt-5 font-serif">
+          Hello, I am currently working as a freelance content writer for
+          GeeksforGeeks, where I develop high-quality technical articles and
+          tutorials on various programming and software development topics.
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Experience;
---
+++ src/components/Recommended.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Recommended = () => {
+  return (
+    <>
+      <div className="bg-[#363C43] text-white mx-12 text-xl">
+        <h1 className="mt-5 font-serif">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Reprehenderit quaerat tempora possimus soluta, eligendi consectetur?
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Recommended;
